3f9fa8e9ed2256efd68e20193b2bf4cddb362e3c
Paulo Ricardo <pauloricardoh@hotmail.com>
2020-12-09 23:23:31 -0300

    Aula 4 catalogo

---
+++ ASPNETCoreCadaDoCodigo/Aulas/Aula1/CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -0,0 +1,34 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+
+        private readonly ApplicationContext contexto;
+
+        public ProdutoRepository(ApplicationContext contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        public IList<Produto> GetProdutos()
+        {
+            return contexto.Set<Produto>().ToList();
+        }
+
+        public void SaveProdutos(List<Livro> livros)
+        {
+            foreach (var i in livros)
+            {
+                contexto.Set<Produto>().Add(new Produto(i.Codigo, i.Nome, i.Preco));
+            }
+
+           // contexto.SaveChanges();
+        }
+    }
+}
--- ASPNETCoreCadaDoCodigo/Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ ASPNETCoreCadaDoCodigo/Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CasaDoCodigo.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -25,7 +26,10 @@ namespace CasaDoCodigo
             services.AddMvc();
 
             string connectionString = Configuration.GetConnectionString("Default");
+            
             services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+            services.AddTransient<IDataService,DataService>();
+            services.AddTransient<IProdutoRepository, ProdutoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,7 +54,7 @@ namespace CasaDoCodigo
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
 
-            serviceProvider.GetService<ApplicationContext>().Database.Migrate();
+            serviceProvider.GetService<IDataService>().InicializaDB();
 
         }
     }
