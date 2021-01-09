504a3b84f1581c5d1e27b35dfc90565c6fca53b8
Paulo Ricardo <pauloricardoh@hotmail.com>
2021-01-09 19:32:27 -0300

    Aula 3 - ViewModel e Objetos de Respostas

--- ASPNETCoreCadaDoCodigo/Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ ASPNETCoreCadaDoCodigo/Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -1,4 +1,5 @@
 ﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Models.ViewModels;
 using CasaDoCodigo.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -35,10 +36,10 @@ namespace CasaDoCodigo.Controllers
             {
                 pedidoRepository.AddItem(codigo);
             }
-           
-           
-            Pedido pedido = pedidoRepository.GetPedido();
-            return View(pedido.Itens);
+
+            List<ItemPedido> itens = pedidoRepository.GetPedido().Itens;
+            CarrinhoViewModel carrinhoViewModel = new CarrinhoViewModel(itens);
+            return base.View(carrinhoViewModel);
         }
 
         public IActionResult Cadastro()
--- ASPNETCoreCadaDoCodigo/Aulas/Aula1/CasaDoCodigo/wwwroot/js/carrinho.js
+++ ASPNETCoreCadaDoCodigo/Aulas/Aula1/CasaDoCodigo/wwwroot/js/carrinho.js
@@ -35,6 +35,9 @@ class Carrinho {
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify(data)
+        }).done(function (response)
+        {
+            location.reload();
         });
     }
 
